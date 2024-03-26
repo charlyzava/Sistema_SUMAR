2eafa9be22b284295a8c6adb464b072314e1b0ef
charlyzava <charly_zava@yahoo.com.ar>
2024-03-26 12:31:16 -0300

    agregué función para buscar Nomenclador por fecha a funciones.php

--- funciones.php
+++ funciones.php
@@ -37,4 +37,33 @@ function obtenerNomenclador($periodo, $conn) {
         return "No se encontró ningún nomenclador para el período $periodo";
     }
 }
+
+// SELECCIONAR NOMENCLADOR SEGUN FECHA PRACTICA formato 01/01/2023
+function obtenerNomencladorFecha($fecha, $conn) {
+    
+    // Consulta SQL para obtener el nomenclador
+    $sql = "SELECT *
+            FROM NOMENCLADOR
+            WHERE '$fecha' >= FECHAINICIO
+            AND ('$fecha' <= FECHAFIN OR FECHAFIN IS NULL)";
+    
+    // Ejecutar la consulta
+    $ressql = sqlsrv_query($conn, $sql);
+    
+    // Verificar si se obtuvieron resultados
+    if ($ressql === false) {
+        return "Error al ejecutar la consulta: " . sqlsrv_errors()[0]['message'];
+    }
+    
+    // Verificar si se encontró un nomenclador
+    if (sqlsrv_has_rows($ressql)) {
+        // Obtener el primer resultado
+        $row2 = sqlsrv_fetch_array($ressql);
+        $nomenclador_result = $row2['NOMBRE'];
+        return trim($nomenclador_result);
+    } else {
+        return "No se encontró ningún nomenclador para el período $periodo";
+    }
+}
+// FIN SELECCIONAR NOMENCLADOR SEGUN FECHA
 ?>
